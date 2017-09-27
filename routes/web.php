c0dd8e3a1e10e1d510261a4d33521ee099e26266
정새롬 <jeong@jeongsaelom-ui-MacBook.local>
2017-09-27 10:26:06 +0900

    #4 edit route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,20 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => '/comments'], function(){
+    Route::post('/{target_table}/{target_id}',[
+        'as' => 'commentsStore',
+        'uses' => 'CommentsController@store'
+    ]);
+
+    Route::post('/{comments_id}', [
+        'as' => 'recommentsStore',
+        'uses' => 'CommentsController@recommentsStore'
+    ]);
+
+    Route::get('/{target_table}/{target_id}', [
+        'as' => 'commentsShow',
+        'uses' => 'CommentsController@index'
+    ]);
+});

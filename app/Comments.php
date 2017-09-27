796d5e490163271b6ff9b5eb2d4a7651ecdaa498
정새롬 <jeong@jeongsaelom-ui-MacBook.local>
2017-09-27 10:20:04 +0900

    #3 edit model

--- app/Comments.php
+++ app/Comments.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comments extends Model
 {
-    //
+    protected $fillable = [
+        'parent_id',
+        'target_table',
+        'target_id',
+        'contents',
+    ];
+
+    protected $dates = [
+        'insert_dt',
+        'update_dt',
+    ];
+
+    /* Relationships */
+    public function commentable(){
+        return $this->morpTo();
+    }
+
+    public function child()
+    {
+        return $this->hasMany(Comments::class, 'parent_id');
+    }
 }
